d8b8964db5047b3fc1318bd1e52e6fce2a883e2f
Muhammad Atif Shakeel <99881369+AtifShakeel@users.noreply.github.com>
2025-07-02 11:51:22 +0500

    Update api.js

--- client/src/config/api.js
+++ client/src/config/api.js
@@ -1,3 +1,3 @@
-const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://mernfood-57b5593638dc.herokuapp.com/' || 'http://localhost:5000/';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'https://mernfood-57b5593638dc.herokuapp.com';
 
 export default API_BASE_URL;
