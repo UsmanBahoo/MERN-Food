9639790aeb996fe48cbc86e18c9ba462c6837f63
UsmanBahoo <usmanbahoo381@gmail.com>
2025-07-03 11:18:37 +0500

    Refactor Header component to manage mobile menu state and improve accessibility

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -6,6 +6,7 @@ import useCart from '../contexts/Cart/UseCart';
 
 const Header = () => {
     const [isOpen, setIsOpen] = useState(false);
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
 
     const { isLoggedIn, logout, user } = useAuth();
     const { getTotalItems, clearCartOnLogout} = useCart();
@@ -97,7 +98,8 @@ const Header = () => {
                 type="button"
                 className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
                 aria-controls="navbar-cta"
-                aria-expanded="false"
+                aria-expanded={isMobileMenuOpen}
+                onClick={() => setIsMobileMenuOpen(!isMobileMenuOpen)}
               >
                 <span className="sr-only">Open main menu</span>
                 <svg
@@ -119,7 +121,7 @@ const Header = () => {
             </div>
   
             <div
-              className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+              className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${isMobileMenuOpen ? 'block' : 'hidden'}`}
               id="navbar-cta"
             >
               <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
