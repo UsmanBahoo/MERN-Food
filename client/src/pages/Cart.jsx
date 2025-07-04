57e431719355f1456e0f3b7607dd9410256f31c4
UsmanBahoo <usmanbahoo381@gmail.com>
2025-07-04 15:52:28 +0500

    Refactor Cart component for improved hover effect and layout responsiveness

--- client/src/pages/Cart.jsx
+++ client/src/pages/Cart.jsx
@@ -48,7 +48,7 @@ const Cart = () => {
               key={item.id}
               className="border rounded-lg p-4 bg-white shadow-lg w-80 relative group"
             >
-              <div className="absolute top-2 left-2 right-2 flex justify-between opacity-0 group-hover:opacity-100 transition-opacity duration-300 ">
+              <div className="absolute top-2 lg:top-3 left-2 lg:left-3 right-2 lg:right-3 flex justify-between opacity-100 lg:opacity-0 lg:group-hover:opacity-100 transition-opacity duration-300 z-10">
                 <Link to={`/quicknow/${item.id}`}>
                   <button className="bg-white p-3 rounded-full shadow-md cursor-pointer">
                     <img src="Svg/eye-solid.svg" alt="View item details" className="w-5 h-5" />
